3b9c09b8c397d69cab3cf84c960e4de2ce1d45ad
impressivecoder1 <rohanrahman251@gmail.com>
2026-06-03 02:46:39 +0600

    sql  table

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,7 +6,27 @@ export const sql = neon(config.database_url)
 
 export const initDB = async()=>{
     await sql `
-    
+    CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    age INT NOT NULL, 
+    role VARCHAR(20) NOT NULL DEFAULT 'user',
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )
     `
+    await sql `
+    CREATE TABLE IF NOT EXISTS orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+    quantity INT NOT NULL CHECK(quantity>0),
+    food TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )
+    `
+
     console.log('database connected');
 }
