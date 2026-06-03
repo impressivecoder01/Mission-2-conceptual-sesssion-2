c7a2f97c90eb9c9623cb93bd67be98fef1cd3290
impressivecoder1 <rohanrahman251@gmail.com>
2026-06-03 22:24:32 +0600

    gemeric  response

---
+++ src/utilis/sendResponse.ts
@@ -0,0 +1,9 @@
+import type { Response } from "express";
+
+export const sendResponse = <T>(res: Response, {message,data,error}: {message: unknown, data?: T, error?: boolean}, status=200): void => {
+    res.status(status).json({
+        success: error? false : true,
+        message: message,
+        data: error?undefined :data
+    })
+}
