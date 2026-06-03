f9387df46b3aa2a426e07b88db4dcd958bd2b197
impressivecoder1 <rohanrahman251@gmail.com>
2026-06-04 01:11:20 +0600

    add logger  middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { type Application, type Request, type Response } from "express"
 import config from "./config";
+import { logger } from "./middleware/logger";
 // console.log('hello');
 const app : Application = express()
+app.use(logger)
 
 app.get("/", (req:Request, res:Response)=>{
     res.send('hello')
